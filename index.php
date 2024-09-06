fd7bc0048da1b641acac53273f83ff71caad9012
Tikapus140306 <tikapuspita844@gmail.com>
2024-09-06 11:15:36 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head lang="en">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Portofolio</title>
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+    />
+
+    
+  </head>
+  <body>
+    <!-- navbar start -->
+    <nav
+      class="navbar navbar-expand-lg bg-success fixed-top navbar-dark shadow"
+    >
+      <div class="container">
+        <a class="navbar-brand fw-bold fs-4" href="#home"
+          ><span class="text-warning fst-italic"></span>Portofolio</a
+        >
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto fw-bold fs-6">
+            <li class="nav-item">
+              <a class="nav-link me-4" aria-current="page" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link me-4" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link me-4" href="#hobby">Hobby</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link me-4" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- navbar end -->
+
+    <!-- hero start -->
+    <section id="home">
+      <div class="container">
+        <div class="row min-vh-100 align-items-center align-content-center">
+          <div class="col-md-6">
+            <div class="home-img text-center">
+              <img
+                src="img/aku.jpg"
+                style="border: 3px solid #198754"
+                class="rounded-circle img-profile mw-100"
+                width="350"
+              />
+            </div>
+          </div>
+          <div class="col-md-6 order-md-first">
+            <div class="home-text">
+              <p class="text-muted mb-1">Hello, Saya</p>
+              <h1 class="text-uppercase fs-1 fw-bold text-success">
+                Tika Puspita Dewi
+              </h1>
+              <h3 class="fs-5">Pelajar | SMK Pangudi Luhur Seputih Mataram</h3>
+              <p class="mt-2 text-muted">Kelas XII RPL 3</p>
+              <p>
+                <a
+                  href="#contact"
+                  class="d-inline-flex focus-ring focus-ring-success bg-success py-2 px-3
+                   text-decoration-none border text-white rounded-5 active"
+                  >Hubungi Saya</a
+                >
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- hero end -->
+
+    <!-- about start -->
+    <section class="pt-5 pb-5" id="about">
+      <div class="container mb-5 py-5">
+        <div class="row text-center">
+          <div class="col">
+            <h2 class="text-success fw-bold fs-1">About</h2>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col">
+            <p class="fw-light flex-wrap">
+              Bisa dilihat riwat hidup saya selama saya hidup dan pendidikan
+              saya yang sedang saya jalani sekarang.
+            </p>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col-6">
+            <h4 class="fw-semibold">Sekilas tentang saya</h4>
+            <p>
+              Nama saya Tika Puspita Dewi
+              <br>Saya tinggal disebuah desa kecil desa Nambah Dadi yang ada di Provinsi Lampung, Kabupaten Lampung Tengah, Kecamatan Terbanggi Besar.
+              <br>Saya lahir di Nambah Dadi pada tanggal 14 Maret 2006
+              <br>Sekarang umur saya 18 tahun
+              Cita-cita saya Adalah DESIGNER.
+            </p>
+          </div>
+          <div class="col-6">
+            <h4 class="fw-semibold">Pendidikan</h4>
+            <p>
+              Untuk Pendidikan pertama saya sekolah di TK DHARMA WANITA di Karang Endah.
+              pendidikan kedua saya masuk sekolah dasar (SD) di SD N 1 Karang Endah. Pendidikan
+              ketiga saya masuk ke Sekolah Menengah Pertama (SMP) di SMP N 6 Terbanggi Besar.
+              Dan sekarang saya sedang menjalani pendidikan SMK sederajat di SMK Pangudi Luhur Seputih Mataram.
+            </p>
+          </div>
+          <div class="col-6">
+            <h4 class="fw-semibold">Pengalaman</h4>
+            <p>
+              Pada tanggal 18 desember 2023 saya pernah melaksanakan Praktik Kerja Lapangan (PKL) di kantor kecamatan Seputih matarm
+              saya PKL selama 3 bulan, Disana saya bersama 2 teman saya yang bernama intan dan devi.
+              tugas saya disana ya itu membantu orang yang mau melaksanakan rekaman KTP dan saya juga membantu para Staf yang ada 
+              di kantor seperti membatu Mengetik di WORD.
+              Saya dan teman-teman selesai melaksanakan PKL pada tanggal 9 Maret 2024.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- about end -->
+
+    <!-- hobi start -->
+    <section class="pt-5 pb-5 bg-body-secondary" id="hobby">
+      <div class="container mb-5 py-5 mt-5">
+        <div class="row text-center">
+          <div class="col">
+            <h2 class="text-success fw-bold fs-1">Hobby</h2>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col">
+            <p class="fw-light flex-wrap">Ini adalahh beberaapa hobi saya.</p>
+          </div>
+        </div>
+
+        <div class="container mt-3 d-flex justify-content-center flex-wrap">
+          <div class="card me-5 shadow mt-3" style="width: 20rem">
+            <div class="card-body">
+              <h5 class="card-title text-center">Memasak</h5>
+              <hr class="border-4 rounded-5" />
+              <p class="card-text">
+              Memasak adalah kegiatan menyiapkan makanan dengan cara mengolah dan memanaskan
+              bahan makanan tersebut bisa dikonsumsi. Memasak terdiri dari berbagai macam metode,teknik,peralatan,dan kombinasi bumbu dapur. 
+              </p>
+            </div>
+          </div>
+
+          <div class="card me-5 shadow mt-3" style="width: 20rem">
+            <div class="card-body">
+              <h5 class="card-title text-center">Menanam bunga</h5>
+              <hr class="border-4 rounded-5" />
+              <p class="card-text">
+                Menanam bunga merupakan pilihan tercepat dan tepat untuk menghiasi halaman rumah
+                warana-warna yang bervariasi dapat dengan mudah meningkatkan keindahan tak hanya itu,
+                bunga yang kamu pilih juga bisa dilestarikan.
+              </p>
+            </div>
+          </div>
+
+          <div class="card me-5 shadow mt-3" style="width: 20rem">
+            <div class="card-body">
+              <h5 class="card-title text-center">melihara kucing</h5>
+              <hr class="border-4 rounded-5" />
+              <p class="card-text">
+                 saya suka merawat kucing karna sifat kesederhanaan dan kemandiriannya,sementara yang lain mungkin
+                 menyukai mereka karena tingkah lucu,kenyamanan,atau hubungan khusus yang dapat di bangun <br />
+              </p>
+            </div>
+          </div>
+          <div class="card me-5 shadow mt-3" style="width: 20rem">
+            <div class="card-body">
+              <h5 class="card-title text-center">Membaca novel</h5>
+              <hr class="border-4 rounded-5" />
+              <p class="card-text">
+                Membaca Novel adalah salah satu kegiatan yang menawarkan lebih dari sekedar hiburan.
+                saat kamu tenggelam dalam alur cerita yang menarik dan karakter yang kompleks, pikiranmu diajak untuk 
+                berimajinasi dan merasakan emosi yang beragam.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- hobi end -->
+
+    
+
+ <!-- contact start -->
+ <section class="pt-5 pb-5 " id="contact">
+      <div class="container mb-5 py-5">
+        <div class="row text-center">
+          <div class="col">
+            <h2 class="text-success fw-bold fs-1">Contact</h2>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col">
+            <p class="fw-light">
+              Anda bisa menghubungi saya lewat beberapa media sosial saya
+              dibawah ini.
+            </p>
+          </div>
+        </div>
+        <div class="row text-center py-4">
+          <div class="col">
+            <!-- instragram -->
+            <a
+              href=""
+              target="_blank"
+              class="btn rounded-5 me-4"
+              style="background-color: #2bc97f"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                class=""
+                viewBox="0 0 24 24"
+                style="fill: rgba(0, 0, 0, 1); transform: msfilter"
+              >
+                <path
+                  d="M20.947 8.30a6.53 6.53 0 0 0-.419-2.216 4.61 4.61 0 0 0-2.633-2.633 6.606 6.606 0 0 0-2.186-.42c-.962-.043-1.267-.055-3.709-.055s-2.755 0-3.71.055a6.606 6.606 0 0 0-2.185.42 4.607 4.607 0 0 0-2.633 2.633 6.554 6.554 0 0 0-.419 2.185c-.043.963-.056 1.268-.056 3.71s0 2.754.056 3.71c.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.043 1.268.056 3.71.056s2.755 0 3.71-.056a6.59 6.59 0 0 0 2.186-.419 4.615 4.615 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.187.043-.962.056-1.267.056-3.71-.002-2.442-.002-2.752-.058-3.709zm-8.953 8.297c-2.554 0-4.623-2.069-4.623-4.623s2.069-4.623 4.623-4.623a4.623 4.623 0 0 1 0 9.246zm4.807-8.339a1.077 1.077 0 0 1-1.078-1.078 1.077 1.077 0 1 1 2.155 0c0 .596-.482 1.078-1.077 1.078z"
+                ></path>
+                <circle cx="11.994" cy="11.979" r="3.003"></circle>
+              </svg>
+            </a>
+
+            <!-- facebook -->
+            <a
+              href=""
+              target="_blank"
+              class="btn rounded-5 me-4"
+              style="background-color: #2bc97f"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                style="fill: rgba(0, 0, 0, 1); transform: msfilter"
+              >
+                <path
+                  d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"
+                ></path>
+              </svg>
+            </a>
+
+            <!-- wa -->
+            <a
+              href=""
+              target="_blank"
+              class="btn rounded-5 me-4"
+              style="background-color: #2bc97f"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                style=""
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M18.403 5.633A8.919 8.919 0 0 0 12.053 3c-4.948 0-8.976 4.027-8.978 8.977 0 1.582.413 3.126 1.198 4.488L3 21.116l4.759-1.249a8.981 8.981 0 0 0 4.29 1.093h.004c4.947 0 8.975-4.027 8.977-8.977a8.926 8.926 0 0 0-2.627-6.35m-6.35 13.812h-.003a7.446 7.446 0 0 1-3.798-1.041l-.272-.162-2.824.741.753-2.753-.177-.282a7.448 7.448 0 0 1-1.141-3.971c.002-4.114 3.349-7.461 7.465-7.461a7.413 7.413 0 0 1 5.275 2.188 7.42 7.42 0 0 1 2.183 5.279c-.002 4.114-3.349 7.462-7.461 7.462m4.093-5.589c-.225-.113-1.327-.655-1.533-.73-.205-.075-.354-.112-.504.112s-.58.729-.711.879-.262.168-.486.056-.947-.349-1.804-1.113c-.667-.595-1.117-1.329-1.248-1.554s-.014-.346.099-.458c.101-.1.224-.262.336-.393.112-.131.149-.224.224-.374s.038-.281-.019-.393c-.056-.113-.505-1.217-.692-1.666-.181-.435-.366-.377-.504-.383a9.65 9.65 0 0 0-.429-.008.826.826 0 0 0-.599.28c-.206.225-.785.767-.785 1.871s.804 2.171.916 2.321c.112.15 1.582 2.415 3.832 3.387.536.231.954.369 1.279.473.537.171 1.026.146 1.413.089.431-.064 1.327-.542 1.514-1.066.187-.524.187-.973.131-1.067-.056-.094-.207-.151-.43-.263"
+                ></path>
+              </svg>
+            </a>
+
+            <!-- tt -->
+            <a
+              href=""
+              target="_blank"
+              class="btn rounded-5"
+              style="background-color: #2bc97f"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                style="fill: rgba(0, 0, 0, 1); transform: msfilter"
+              >
+                <path
+                  d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- contact end -->
+
+
+    <!-- Contact Me -->
+    <section class="pt-5 pb-5 " id="contact">
+      <div class="container mb-5 py-5">
+        <div class="row text-center">
+          <div class="col">
+            
+
+        <h2></h2>
+      </div>
+    </div>
+  <div class="row justify-content-center">
+  <div class="col">
+<form>
+  <div class="mb-3">
+    <label for="name" class="form-label">Nama Lengkap</label>
+<input type="text" class="form-control" id="name" aria-describedby="name">
+</div>
+<div class="mb-3">
+  <label for="email" class="form-label">Email</label>
+<input type="email" class="form-control" id="email" aria-describedby="email">
+      </div>
+    <div class="mb-3">
+  <label for="pesan" class="form-label">Pesan</label>
+<textarea id="pesan" class="form-control" rows="3"></textarea>
+</div>
+<button type="submit" class="btn btn-success">Submit</button>
+</form>
+</div>
+</div>
+</div>
+</section>
+<!-- Akhir Contact Me -->
+
+
+    <!-- footer start -->
+    <footer class="bg-success p-4">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <p class="text-white fw-semibold pt-2">
+              Created by Tika Puspita Dewi &copy; 2024
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- footer end -->
+    <script src="js/bootstrap.bundle.js"></script>
+
+    <script>
+      const active = document.querySelectorAll(".nav-link");
+      for (let i = 0; i < active.length; i++) {
+        active[i].addEventListener("click", function () {
+          const item = document.getElementsByClassName("active");
+          if (item.length > 0) {
+            item[0].className = item[0].className.replace(" active", "");
+          }
+          this.className += " active";
+        });
+      }
+    </script>
+  </body>
+</html>
